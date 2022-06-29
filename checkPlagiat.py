3192f9fa8f026b1b2fa57252b7320dc81219b337
930C <93C@pm.me>
2022-06-29 21:04:32 +0200

    fixed return statements

--- checkPlagiat.py
+++ checkPlagiat.py
@@ -7,3 +7,5 @@ def checkPlagiat(sentence, html, url):
     if find > -1:
         return "Plagiat " + sentence + " an Stelle " + str(find) + " von " + url
         # return true # TODO in regex verfeinerte Suche nach Plagiat (z.B. Kommatrennung)
+    else:
+        return ""
