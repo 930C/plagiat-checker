cf34b08d173376ea56c8ae0b01db7d853f92c9db
930C <93C@pm.me>
2022-06-29 21:22:50 +0200

    added " " to the search

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import checkPlagiat as cp
 content = rf.readFile("./testfiles/mytext.txt")
 
 for i in content:
-    for j in search(i, tld="de", num=4, stop=10, pause=2):
+    for j in search('"' + i + '"', tld="de", num=4, stop=10, pause=2):
         try:
             clean_html = rw.readWebsite(j)
             print(cp.checkPlagiat(i, clean_html, j))
